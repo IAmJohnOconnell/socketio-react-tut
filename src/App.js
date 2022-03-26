a50263c98f83bdcd94df1df649e11edc9f334176
John O'Connell <johnathano619@gmail.com>
2022-03-26 17:23:31 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import io from "socket.io-client"
+import { useEffect, useState } from "react"
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [room, setRoom] = useState("")
+	const [message, setMessage] = useState("")
+	const [messageRecieved, setMessageRecieved] = useState("")
+
+	const sendMessage = () => {
+		socket.emit("send_message", { message, room })
+	}
+
+	const joinRoom = () => {
+		if (room !== "") {
+			socket.emit("join_room", room)
+		}
+	}
+
+	useEffect(() => {
+		socket.on("recieve_message", (data) => {
+			setMessageRecieved(data.message)
+		})
+	}, [socket])
+
+	return (
+		<div className='App'>
+			<div>
+				<input
+					type='text'
+					placeholder='Room'
+					onChange={(e) => {
+						setRoom(e.target.value)
+					}}
+				/>
+				<button onClick={joinRoom}>Join Room</button>
+			</div>
+			<div>
+				<input
+					type='text'
+					placeholder='Message...'
+					onChange={(e) => {
+						setMessage(e.target.value)
+					}}
+				/>
+				<button onClick={sendMessage}>Send Message</button>
+			</div>
+			<h1>
+				Message:
+				{messageRecieved}
+			</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
